0b9da27fb926252ffb19022a4b4233b5da39974b
Braydon <burrow.braydon14@gmail.com>
2020-04-23 15:10:33 -0600

    Update Python_Drill_Script2.py

--- Python_Drill_Script2.py
+++ Python_Drill_Script2.py
@@ -1,6 +1,8 @@
 
 import sqlite3
 
+import os
+
 conn = sqlite3.connect('Drill.db')
 
 with conn:
@@ -12,5 +14,29 @@ with conn:
     conn.commit()
 conn.close()
 
+fileList = ('information.docx', 'Hello.txt','myImage.png', \
+            'myMovie.mpg', 'World.txt', 'data.pdf', 'myPhoto.jpg')
 
-      
+conn = sqlite3.connect('Drill.db')
+
+for filename in fileList:
+    if filename.endswith(".txt"):
+        with conn:
+            cur = conn.cursor()
+            cur.execute("INSERT INTO tbl_file(Col_txt) VALUES (?)", \
+                        ('Hello'))
+            cur.execute("INSERT INTO tbl_file(Col_txt) VALUES (?)", \
+                        ('World'))
+            conn.commit()
+        conn.close()
+        print(filename)
+
+
+with conn:
+    cur = conn.cursor()
+    cur.execute("SELECT Col_txt FROM tbl_file WHERE Col_txt = 'Hello'")
+    cur.execute("SELECT Col_txt FROM tbl_file WHERE Col_txt = 'World'")
+    varFile = cur.fetchall()
+    for item in varFile:
+        msg = "File Name: {}".format(item[0])
+    print(msg)
