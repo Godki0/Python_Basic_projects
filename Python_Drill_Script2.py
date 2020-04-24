0c5e16db25ea13747ad5aa554ae9be7da50e2c45
Braydon <burrow.braydon14@gmail.com>
2020-04-23 22:30:20 -0600

    Update Python_Drill_Script2.py

--- Python_Drill_Script2.py
+++ Python_Drill_Script2.py
@@ -1,8 +1,6 @@
 
 import sqlite3
 
-import os
-
 conn = sqlite3.connect('Drill.db')
 
 with conn:
@@ -24,19 +22,9 @@ for filename in fileList:
         with conn:
             cur = conn.cursor()
             cur.execute("INSERT INTO tbl_file(Col_txt) VALUES (?)", \
-                        ('Hello'))
-            cur.execute("INSERT INTO tbl_file(Col_txt) VALUES (?)", \
-                        ('World'))
+                        ('filename'))
             conn.commit()
-        conn.close()
         print(filename)
 
 
-with conn:
-    cur = conn.cursor()
-    cur.execute("SELECT Col_txt FROM tbl_file WHERE Col_txt = 'Hello'")
-    cur.execute("SELECT Col_txt FROM tbl_file WHERE Col_txt = 'World'")
-    varFile = cur.fetchall()
-    for item in varFile:
-        msg = "File Name: {}".format(item[0])
-    print(msg)
+
