c9d0f1e14eb654a4feb95517c026b3fdf10dfd5b
Braydon <burrow.braydon14@gmail.com>
2020-05-03 17:20:22 -0600

    testing code

---
+++ testCody3.py
@@ -0,0 +1,17 @@
+
+# This is commenting.
+
+""" This is my note
+and still my note
+and again still notes.
+"""
+
+## this is a line of code
+
+def printMe():
+    """ This is my doc string
+    """
+
+    print("Meeeeeee")
+
+print(printMe.__doc__)
